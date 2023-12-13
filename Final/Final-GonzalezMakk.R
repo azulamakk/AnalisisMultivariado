23ae05d8671da966da9483ee7d380f932f03377a
azulamakk <92961593+azulamakk@users.noreply.github.com>
2023-12-13 20:51:09 -0300

    distancias base KNN. Eucli, City Block, Manhala, Chevychev, Canberra

--- Final/Final-GonzalezMakk.R
+++ Final/Final-GonzalezMakk.R
@@ -1,37 +1,62 @@
 library(dplyr)
-library(ggplot2)
+library(caret)
 
-student <- read.csv("~/Universidad/Analisis Multivariado/Final/student-mat.csv")
-View(student)
+calculoIndicadores <- function(predicciones, etiquetas) {
+  # Crear una matriz de confusión
+  confusion_matrix <- confusionMatrix(predicciones, etiquetas)
+  
+  # Calcular Accuracy
+  accuracy <- confusion_matrix$overall["Accuracy"]
+  
+  # Calcular Precision
+  precision <- confusion_matrix$byClass["Precision"]
+  
+  # Calcular Recall (Sensitivity)
+  recall <- confusion_matrix$byClass["Sensitivity"]
+  
+  # Calcular F1 Score
+  f1_score <- confusion_matrix$byClass["F1"]
+  
+  # Crear un vector con los resultados
+  indicadores <- c("Accuracy" = accuracy, "Precision" = precision, "Recall" = recall, "F1 Score" = f1_score)
+  
+  return(indicadores)
+}
 
+set.seed(13)
+data(iris)
+data <- iris %>% select(Sepal.Length, Petal.Length, Species)
 
-# 1 --------------- Seleccion de variables
-data <- student %>% select(address, studytime, internet, absences, Dalc, goout, G3)
+# Crear partición de datos
+split_index <- createDataPartition(data$Species, p = 0.8, list = FALSE)
+train <- data[split_index, ]
+test <- data[-split_index, ]
 
-# 2 --------------- Analisis descriptivo 
+# Definir el espacio de parámetros
+grid <- expand.grid(k = 1:10)
 
-# 2.1 --------------- Datos Numericos
+# Configurar el control de entrenamiento con 5-fold cross-validation
+ctrl <- trainControl(method = "cv", number = 5)
 
-# 2.1.1 --------------- studytime
+# Realizar grid search con cross-validation
+model <- train(Species ~ Sepal.Length + Petal.Length, 
+               data = train, 
+               method = "knn", 
+               trControl = ctrl,
+               tuneGrid = grid)
 
+# Obtener los resultados
+print(model)
 
-# 2.1.2 --------------- absenses 
+# Hacer predicciones con el mejor modelo
+predictions <- predict(model, newdata = test)
 
+# Calcular indicadores de rendimiento
+indicadores <- calculoIndicadores(predictions, test$Species)
 
-# 2.1.3 --------------- Dalc
+cat("Best k:", model$bestTune$k, "\n")
+cat("Accuracy:", indicadores["Accuracy"], "\n")
+cat("Precision:", indicadores["Precision"], "\n")
+cat("Recall:", indicadores["Recall"], "\n")
+cat("F1 Score:", indicadores["F1 Score"], "\n")
 
-
-# 2.1.4 --------------- goout
-
-
-# 2.1.5 --------------- G3
-
-
-
-# 2.2 --------------- Datos Categoricos
-
-
-# 2.2.1 --------------- address
-
-
-# 2.2.2 --------------- internet
